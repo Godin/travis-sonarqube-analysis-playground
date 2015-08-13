029a6059b3bbf8163ef4330b75ec815c5c011382
Evgeny Mandrikov <mandrikov@gmail.com>
2015-08-13 17:40:15 +0200

    Auto-deploy gh-pages

--- travis.sh
+++ travis.sh
@@ -8,3 +8,15 @@ mvn org.codehaus.mojo:sonar-maven-plugin:2.6:sonar \
     -Dsonar.analysis.mode=preview \
     -Dsonar.host.url=http://nemo.sonarqube.org/ \
     -Dsonar.issuesReport.html.enable
+
+rm -rf out || true
+mkdir out
+cp target/sonar/issues-report/issues-report.html out/index.html
+cp -r target/sonar/issues-report/issuesreport_files out/
+cd out
+git init
+git config user.name "Travis CI"
+git config user.email "<you>@<your-email>"
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/Godin/travis-sonarqube-analysis-playground.git" master:gh-pages > /dev/null 2>&1
