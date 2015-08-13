224dac22846cf571e5382df6f339b201a4c53413
Evgeny Mandrikov <mandrikov@gmail.com>
2015-08-13 17:01:37 +0200

    Execute SonarQube analysis in preview mode

--- travis.sh
+++ travis.sh
@@ -3,3 +3,8 @@
 set -euo pipefail
 
 mvn -V -B -e verify
+
+mvn org.codehaus.mojo:sonar-maven-plugin:2.6:sonar \
+    -Dsonar.analysis.mode=preview \
+    -Dsonar.host.url=http://nemo.sonarqube.org/ \
+    -Dsonar.issuesReport.html.enable
